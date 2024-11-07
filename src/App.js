c6e264d9f3bf24b7bd34064c81efbfc710485def
AnastasiiaRubikD <113940204+AnastasiiaRubikD@users.noreply.github.com>
2024-11-07 00:35:55 -0600

    Added repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 import "./App.css";
 
 function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <p>
+        This site was coded by Anastasiia Rubik and is{" "}
+        <a
+          href="https://github.com/AnastasiiaRubikD/react-forecast-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </p>
+    </div>
+  );
 }
 
 export default App;

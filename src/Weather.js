868fdc912d2253fb378a3b6059b7519bc75ebd1d
AnastasiiaRubikD <113940204+AnastasiiaRubikD@users.noreply.github.com>
2024-11-07 02:00:12 -0600

    Added basic styling

---
+++ src/Weather.js
@@ -0,0 +1,51 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
+        <div className="row">
+          <div className="col-9">
+            <input
+              type="search"
+              placeholder="Enter a city..."
+              className="form-control"
+              autoFocus="on"
+            />
+          </div>
+          <div className="col-3">
+            <button className="btn btn-primary w-100">Search</button>
+          </div>
+        </div>
+      </form>
+      <h1>Florence</h1>
+      <ul>
+        <li>Thursday 1:00</li>
+        <li>Mostly cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="clearfix">
+            <img
+              src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
+              alt="cloudy"
+              className="float-start"
+            />
+            <div className="float-start">
+              <span className="temp">4</span>
+              <span className="unit">°C</span>
+            </div>
+          </div>
+        </div>
+        <div className="col-6">
+          <ul>
+            <li>Precipitation: 2%</li>
+            <li>Humidity: 94%</li>
+            <li>Wind: 5 km/h</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}

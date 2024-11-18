8de53750fd98053ac822c8f27d8962f04c9f7164
AnastasiiaRubikD <113940204+AnastasiiaRubikD@users.noreply.github.com>
2024-11-17 23:49:18 -0600

    Added API integration

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App container">
-      <Weather />
+      <Weather defaultCity="Los Angeles" />
       <footer>
         This site was coded by Anastasiia Rubik and is{" "}
         <a
